e31958a64237c87e315f618478fb3acdf69c0d25
szymsow <rambotracer@gmail.com>
2022-06-15 20:47:54 +0200

    Added DbContext

---
+++ PaginationAPI/WebAPI/Context/PaginationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Context
+{
+    public class PaginationDbContext : DbContext
+    {
+        public PaginationDbContext(DbContextOptions<PaginationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
