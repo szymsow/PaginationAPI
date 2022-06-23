42b9ec9600a86994479252f63dd3301514b94f1e
szymsow <rambotracer@gmail.com>
2022-06-24 00:06:40 +0200

    Added Models and MappingProfile

---
+++ PaginationAPI/WebAPI/Mapping/PaginationMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebAPI.Entities;
+using WebAPI.Models;
+
+namespace WebAPI.Mapping
+{
+    public class PaginationMappingProfile : Profile
+    {
+        public PaginationMappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>();
+        }
+    }
+}
--- PaginationAPI/WebAPI/Program.cs
+++ PaginationAPI/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using WebAPI;
 using WebAPI.Context;
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<Seeder>();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddDbContext<PaginationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
